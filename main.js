96f703eff366e4c284875c42d951173e264c7a21
Jimmy Lin <jimmydlin@gmail.com>
2019-10-18 14:35:56 -0700

    menu formatting v2

--- main.js
+++ main.js
@@ -22,14 +22,54 @@ $(document).ready(function(){
             // const description=appetizers.description
             const html = data.appetizers.map(item => (
              `
-            name: ${item.name}
-            price: $${item.price}
-            description: ${item.description}
+            name: ${item.name}<br>
+            price: $${item.price}<br>
+            description: ${item.description}<br>
+            <br>
             `
             ))
             
 
-            $("#menuapi").html(html)
+            $("#appapi").html(html)
         })
 
+        $.get("https://obscure-tundra-54269.herokuapp.com/bar-food")
+        .done(function(data){
+            // console.log(data)
+            // const result = data.appetizers[0]
+            // const name=appetizers.name
+            // const price=appetizers.price
+            // const description=appetizers.description
+            const html = data.entrees.map(item => (
+             `
+            name: ${item.name}<br>
+            price: $${item.price}<br>
+            description: ${item.description}<br>
+            <br>
+            `
+            ))
+            
+
+            $("#entreesapi").html(html)
+        })
+
+        $.get("https://obscure-tundra-54269.herokuapp.com/bar-food")
+        .done(function(data){
+            // console.log(data)
+            // const result = data.appetizers[0]
+            // const name=appetizers.name
+            // const price=appetizers.price
+            // const description=appetizers.description
+            const html = data.desserts.map(item => (
+             `
+            name: ${item.name}<br>
+            price: $${item.price}<br>
+            description: ${item.description}<br>
+            <br>
+            `
+            ))
+            
+
+            $("#dessertsapi").html(html)
+        })
 })
